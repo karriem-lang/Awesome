2e34518cc769aa20ca4136e3a6578d9a48e1218b
karriem-lang <karriemlang@proton.me>
2025-02-25 23:26:00 -0500

    Initial Commit

---
+++ Awesome/AwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeApp.swift
+//  Awesome
+//
+//  Created by Karriem Lang on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
